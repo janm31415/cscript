7a9e64bfeb2305668a948d7de2a00a08fab3ef50
Jan Maes <janm31415@gmail.com>
2023-02-28 15:30:22 +0100

    flonum tests

--- cscript.tests/compiler_tests.c
+++ cscript.tests/compiler_tests.c
@@ -36,7 +36,36 @@ static void test_compile_fixnum()
   test_compile_fixnum_aux(20, "20;");
   }
 
+static void test_compile_flonum_aux(cscript_flonum expected, const char* script)
+  {
+  cscript_context* ctxt = cscript_open(256);
+  cscript_vector tokens = cscript_script2tokens(ctxt, script);
+  cscript_program prog = make_program(ctxt, &tokens);
+
+  cscript_vector compiled_program = cscript_compile_program(ctxt, &prog);
+  cscript_flonum* res = cast(cscript_flonum*, cscript_run_program(ctxt, &compiled_program));
+
+  TEST_EQ_DOUBLE(expected, *res);
+
+  TEST_EQ_INT(0, ctxt->number_of_compile_errors);
+  TEST_EQ_INT(0, ctxt->number_of_syntax_errors);
+  TEST_EQ_INT(0, ctxt->number_of_runtime_errors);
+
+  cscript_compiled_program_destroy(ctxt, &compiled_program);
+  destroy_tokens_vector(ctxt, &tokens);
+  cscript_program_destroy(ctxt, &prog);
+  cscript_close(ctxt);
+  }
+
+static void test_compile_flonum()
+  {
+  test_compile_flonum_aux(5.1, "5.1;");
+  test_compile_flonum_aux(3.14159, "3.14159;");
+  test_compile_flonum_aux(3.0, "1.0; 2.0; 3.0;");
+  }
+
 void run_all_compiler_tests()
   {
   test_compile_fixnum();
+  test_compile_flonum();
   }
